88368f28a838daff2a95725272b5ba6c5e4fe689
Yeonggyun Kim <kygha7205@naver.com>
2020-03-03 04:53:45 +0900

    Change regexp algorithm due to not to search non-related contents

--- parseAddress.py
+++ parseAddress.py
@@ -3,9 +3,9 @@ import re
 
 def parseAddress(content):
 
-    p = re.compile(
-        '(((대치|삼성) ?(\d*?)? ?동) ?((\d*)? ?(-?) ?(\d*)?(번지)?))|(([가-힣]*(길|로)).(\d*.)?(길|로)?.(\d*).(\d*))|(고려.?.?)')
-    # p = re.compile('(.대치.)|(.삼성.)')
+    # p = re.compile(
+        # '(((대치|삼성) ?(\d*?)? ?동) ?((\d*)? ?(-?) ?(\d*)?(번지)?))|(([가-힣]*(길|로)).(\d*.)?(길|로)?.(\d*).(\d*))|(고려.?.?)')
+    p = re.compile('(.대치.)|(.삼성.)|(.고려.)')
     reg_res = p.search(content)
     if reg_res:
         m = reg_res.group()
