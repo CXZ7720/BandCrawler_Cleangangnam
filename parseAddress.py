a73756adb4f1543c909d0618ccd9fe176d27c0c2
Yeonggyun Kim <kygha7205@naver.com>
2020-03-03 17:09:22 +0900

    Regular Expression edited

--- parseAddress.py
+++ parseAddress.py
@@ -5,11 +5,16 @@ def parseAddress(content):
     # @TODO : 정규표현식 개선필요.
     # p = re.compile(
         # '(((대치|삼성) ?(\d*?)? ?동) ?((\d*)? ?(-?) ?(\d*)?(번지)?))|(([가-힣]*(길|로)).(\d*.)?(길|로)?.(\d*).(\d*))|(고려.?.?)')
-    p = re.compile('(대치)|(삼성)|(봉은사)|(선릉)|(학동)|(도곡)|(영동대로)|(역삼로.?)|(테헤란로?)|(.고려.)')
+    p = re.compile('(대치로 ?(\d{0,5})?)|(삼성로 ?(\d{0,5})?길?)|(봉은사로 ?(\d{0,5})?)|(선릉로 ?(\d{0,5})?길?)|(학동로 ?(\d{0,5})?길?)|(도곡로 ?(\d{0,5})?길?)|(영동대로 ?(\d{0,5})길?)|(역삼로 ?(\d{0,5})?길?)|(테헤란로 ?(\d{0,5})?길?)|(.고려.)')
+    q = re.compile('(성진)|(상록수)|(웅비)|(하진)|(하남)|(로얄)|(태화)|(평원)')# 예외조건
     reg_res = p.search(content)
+    except_res = q.search(content)
     if reg_res:
         m = reg_res.group()
-        print(m)
+        if except_res: # 예외조건에 해당하는 경우 None 리턴
+            print("Not our business!!")
+            return None
+        # print(m)
         return m
     else:
         return None
