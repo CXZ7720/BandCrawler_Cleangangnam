f5bb198da51688f6c7d7c8ebf3aa37df87fa4fd6
Yeonggyun.Kim <kygha7205@naver.com>
2020-03-11 16:48:23 +0900

    Edit env for github actions

--- env.py
+++ env.py
@@ -9,11 +9,23 @@ def openenv():
 def getenv(file):
     env = {}
     lines = file.readlines() # 줄별로 읽어서 배열로 리턴
-    env['band_token'] = lines[0][:-1]
-    env['db_usr'] = lines[1][:-1]
-    env['db_pwd'] = lines[2][:-1]
-    env['db_name'] = lines[3][:-1]
-    env['telegram_token'] = lines[4][:-1]
-    env['channel_id'] = lines[5]
+
+    # for Github Actions
+    env['band_token'] = os.getenv('band_token')
+    env['db_usr'] = os.getenv('db_user')
+    env['db_pwd'] = os.getenv('db_pwd')
+    env['db_name'] = os.getenv('db_name')
+    env['telegram_token'] = os.getenv('telegram_token')
+    env['channel_id'] = os.getenv('channel_id')
+
+    if None in env.values():
+        print("OS env not fund. Using local file value")
+        env['band_token'] = lines[0][:-1]
+        env['db_usr'] = lines[1][:-1]
+        env['db_pwd'] = lines[2][:-1]
+        env['db_name'] = lines[3][:-1]
+        env['telegram_token'] = lines[4][:-1]
+        env['channel_id'] = lines[5]
 
     return env
+
